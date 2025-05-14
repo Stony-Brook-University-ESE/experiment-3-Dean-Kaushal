22169b5421d1a2229400beec2f9f800e7df39cd2
Dean-Kaushal <deankaushal33@gmail.com>
2025-05-14 13:13:26 -0400

    couldnt solve

--- Build/Experiment3.c
+++ Build/Experiment3.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int n = 1000;
+    int n = 10;
     int matrix[n][n];
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             matrix[i][j] = i * n + j + 1;
+            printf("debug");
         }
     }
 
     for (int i = n - 1; i >= 0; i--) {
         for (int j = n - 1; j >= 0; j--) {
-            printf("%4d ", matrix[i][j]);
+            printf("%6d ", matrix[i][j]);
         }
         printf("\n");
     }
